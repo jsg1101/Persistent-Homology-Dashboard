006f7ff63e29b736c7b10cd86237cceb06f88df7
jsg1101 <43864880+jsg1101@users.noreply.github.com>
2026-06-25 09:11:48 -0600

    Example NAvigation

--- static/JS/navigation.js
+++ static/JS/navigation.js
@@ -29,23 +29,17 @@ navItems.forEach(item => {
         activePage.classList.add("active-page");
       }
 
-      if (pageName === "examples") {
-        initExamplePlots();
-      }
-
       // if (pageName === "examples") {
-
-      //   requestAnimationFrame(() => {
-      //       requestAnimationFrame(() => {
-      //           document.querySelectorAll(".plotly-container").forEach(container => {
-      //               if (container.__plotly__) {
-      //                   Plotly.Plots.resize(container);
-      //               }
-      //           });
-      //       });
-      //   });
-    
+      //   initExamplePlots();
       // }
+
+      if (pageName === "examples") {
+        requestAnimationFrame(() => {
+            requestAnimationFrame(() => {
+                initExamplePlots();
+            });
+        });
+      }
 
       document.body.scrollTo({
         top: 0,
